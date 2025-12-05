b95602d5fd5d27d1ac4318b429fee6ed6c3fa2dc
christinashen <christinashen@localhost>
2025-12-05 13:12:02 -0500

    initial code drop

---
+++ CulminatingSarah/src/Driver.java
@@ -0,0 +1,12 @@
+
+public class Driver {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList<Card> card = new ArrayList<>();
+		System.out.println(cards.size());
+		
+
+	}
+
+}
---
+++ CulminatingSarah/src/GUIDriver.java
@@ -0,0 +1,24 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class GUIDriver extends Application {
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		VBox root = new VBox(10);
+		Label text = new Label("HELLO");
+		root.getChildren().add(text);
+		Scene scene = new Scene(root,600,600);
+		stage.setScene(scene);
+		stage.show();
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+
+	}
+
+}

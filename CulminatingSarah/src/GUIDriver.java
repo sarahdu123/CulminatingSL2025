f9ec91846be853d93dbddeea2447801e101b3615
lexikneis <lexikneis@localhost>
2025-12-11 15:12:11 -0500

    Hey

--- CulminatingSarah/src/GUIDriver.java
+++ CulminatingSarah/src/GUIDriver.java
@@ -10,6 +10,7 @@ public class GUIDriver extends Application {
 	public void start(Stage stage) throws Exception {
 		VBox root = new VBox(10);
 		Label text = new Label("HELLO THERE LEXI");
+		Label text = new Label("HI");
 		root.getChildren().add(text);
 		Scene scene = new Scene(root,600,600);
 		stage.setScene(scene);

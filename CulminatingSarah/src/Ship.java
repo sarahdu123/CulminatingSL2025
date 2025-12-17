dd2e56476ae9b832fba0d193684c1210ab0e4320
christinashen <christinashen@localhost>
2025-12-17 13:44:08 -0500

    ship + cell update

---
+++ CulminatingSarah/src/Ship.java
@@ -0,0 +1,50 @@
+import java.util.ArrayList;
+
+/**
+ * Ship represents a single Battleship ship Carrier, Destroyer, etc.).
+ * 
+ * We track: 
+ * 	- name and length (metadata)
+ * 	- positions (which coordinates the ship occupies)
+ *  - hits (how many times it has been hit)
+ *  
+ *  Board is responsible for:
+ *  - placing ships
+ *  - determining if a shot hit which ship
+ */
+public class Ship {
+
+	private String name;
+	private int length;
+	
+	// How many times this ship has been hit
+	private int hits;
+	
+	// The exact coordinates this ship occupies
+	private ArrayList<Coordinate> positions;
+	
+	/**
+	 * Constructs a new Ship. 
+	 * 
+	 * @param name ship name
+	 * @param length shhip length (number of grid squares)
+	 */
+	public Ship(String name, int length) {
+		this.name = name; 
+		this.length = length;
+		this.hits = 0;
+		this.hits = 0;
+		this.positions = new ArrayList<>();
+	}
+	
+	/**
+	 * @return ship name
+	 */
+	public String getame() {
+		return name;
+	}
+	
+	/**
+	 * @return ship length
+	 */
+}

7cad7cd25db4ec20b7bb3e8b8fc355b28d8a4560
christinashen <christinashen@localhost>
2025-12-18 22:22:42 -0500

    Merge branch 'main' of https://github.com/sarahdu123/CulminatingSL2025.git into main

--- CulminatingSarah/src/Coordinate.java
+++ CulminatingSarah/src/Coordinate.java
@@ -10,9 +10,11 @@ public class Coordinate {
 		 * @param row 0-based row index
 		 * @param col 0-based column index
 		 */
+		
+		
 
 		public class Coordinate(int row, int col) {
-			this.row = row;
+			this.row = row; 
 			this.col = col;
 
 		}
